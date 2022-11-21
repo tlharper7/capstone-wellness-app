f88ddffa25a70ba9d6a3b08a14f412d3ac714265
b-nelson <brandon.nelson.tech@gmail.com>
2022-11-20 23:46:37 -0600

    removed vue, add vite, add indexdb

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,9 +1,20 @@
+import { addData, getDataById, deleteDataById, getData,profileDataList } from "../js/database";
+
+
 document.getElementById("profilesub").addEventListener("click", function(e) {
     e.preventDefault();
+    console.log("on profile page");
     var fName = document.getElementById("fName").value;
     var lName = document.getElementById("lName").value;
     var eAddy = document.getElementById('eAddy').value;
     var bDate = document.getElementById("bDate").value;
     console.log(bDate);
+    localStorage.setItem("curremailid", eAddy);
+    var data = { "emailID": eAddy+"_profile", "fname": fName, "lname": lName, "bdate": bDate };
+    console.log(data);
+    profileDataList.set(eAddy, data); 
+    addData(data)
     window.location.href = "checkin.html";
-})
+  
+});
+
