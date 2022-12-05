3ca357baf9c3892ee46584f2f4aebac89bbbd175
b-nelson <brandon.nelson.tech@gmail.com>
2022-12-05 16:46:40 -0600

    corrections for vite build

--- vite.config.js
+++ vite.config.js
@@ -3,40 +3,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     build: {
-        outDir: 'dist',
-    rollupOptions: {
-        build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+
             },
            
-            input: {
-                main: resolve(__dirname, 'homepage.html'),
-            },
-
-            input: {
-                main: resolve(__dirname, 'profile.html'),
-            },
-
-            input: {
-                main: resolve(__dirname, 'checkin.html'),
-            },
-
-            input: {
-                main: resolve(__dirname, 'calendar.html'),
-            },
-
-            input: {
-                main: resolve(__dirname, 'charts.html'),
-            },
-            
-            input: {
-                main: resolve(__dirname, 'faq.html'),
-            },
-        
-            },
-        },  
-    },
-},
+        },
+    },  
 })
